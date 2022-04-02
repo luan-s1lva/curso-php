66b14e4b2ad98c7891fb07a2abe0395321f033f1
luan.carmo <luan.carmo@escolar.ifrn.edu.br>
2022-04-02 12:22:36 -0300

    Colocando armadilhas de restrição contra ataques

--- form.php
+++ form.php
@@ -5,22 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORMULÁRIO PARA INSCRIÇÃO DOS COMPETIDORES</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <h1>FORMULÁRIO DE INSCRIÇÃO</h1>
+
+<div class='container'>
+    <h1 class='jumbotron text-center'>FORMULÁRIO DE INSCRIÇÃO</h1>
 
     <form action="natacao.php" method="POST">
-        <label for="nome">SEU NOME:</label>
-        <input type="text" name="nome" />
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="floatingInput" name="nome">
+            <label for="floatingInput">Name</label>
+        </div>
+
+        <div class="form-floating">
+            <input type="text" class="form-control" id="floatingPassword" name="idade">
+            <label for="floatingPassword">Age</label>
+        </div>
         
-        <br>
-
-        <label for="idade">SUA IDADE:</label>
-        <input type="text" name="idade" />
-
         <br>
         
         <button type="submit">ENVIAR</button>
     </form>
+</div>
 </body>
 </html>
--- natacao.php
+++ natacao.php
@@ -5,17 +5,28 @@ $categorias = ['Infantil', 'Adolescente', 'Adulto'];
 $nome = $_POST['nome'];
 $idade = (int) $_POST['idade'];
 
-//var_dump($nome);
 //var_dump($idade);
-
-switch($idade){
-    case $idade > 5 and $idade < 13:
-        echo "$nome irá competir na categoria $categorias[0]";
-        break;
-    case $idade > 12 and $idade < 19:
-        echo "$nome irá competir na categoria $categorias[1]";
-        break;
-    case $idade > 18:
-        echo "$nome irá competir na categoria $categorias[2]";
+if($nome == null or strlen($nome) < 3 or strlen($nome) > 11)
+{
+    echo 'Nome não permitido. Tente outra vez!';
+    return 0;
+}
+else if($idade > 80 or $idade < 5)
+{
+    echo 'Idade não permitida';
+    return 0;
+}
+else{
+    switch($idade){
+        case $idade > 5 and $idade < 13:
+            echo "$nome irá competir na categoria $categorias[0]";
+            break;
+        case $idade > 12 and $idade < 19:
+            echo "$nome irá competir na categoria $categorias[1]";
+            break;
+        case $idade > 18:
+            echo "$nome irá competir na categoria $categorias[2]";
+            break;
+    }
 }
 ?>
